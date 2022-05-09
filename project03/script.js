841ff2c7d9acd342cda439ff7888c809ecf16aef
juliekim456 <97996892+juliekim456@users.noreply.github.com>
2022-05-09 03:28:32 -0400

    new

--- project03/script.js
+++ project03/script.js
@@ -1,30 +1,293 @@
 const textures = [
     {
-      name: "Tulip",
-      color: "yellow",
-      image:
-        "https://cdn.britannica.com/37/227037-050-CA792866/Broken-tulip-flower.jpg"
+      name: "Black and White 1",
+      color: "blackwhite",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066708/Images%20for%20project%203/bw1_zodpqd.jpg"
     },
     {
-      name: "Daffodil",
-      color: "yellow",
-      image: "https://h2.commercev3.net/cdn.brecks.com/images/800/67248A.jpg"
+      name: "Black and White 2",
+      color: "blackwhite",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066714/Images%20for%20project%203/bw2_iplqvq.jpg"
     },
     {
-      name: "Sunflower",
-      color: "yellow",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Sunflower_sky_backdrop.jpg/800px-Sunflower_sky_backdrop.jpg"
+      name: "Black and White 3",
+      color: "blackwhite",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066709/Images%20for%20project%203/bw3_r7wexl.jpg"
     },
     {
-      name: "Bluebell",
+      name: "Black and White 4",
+      color: "blackwhite",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066711/Images%20for%20project%203/bw4_vmhp7r.jpg"
+    },
+    {
+      name: "Black and White 5",
+      color: "blackwhite",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066708/Images%20for%20project%203/bw5_szcufn.jpg"
+    },
+    {
+      name: "Black and White 6",
+      color: "blackwhite",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066709/Images%20for%20project%203/bw6_l3rzgq.jpg"
+    },
+    {
+      name: "Black and White 7",
+      color: "blackwhite",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066708/Images%20for%20project%203/bw7_uv4rxy.jpg"
+    },
+    {
+      name: "Black and White 8",
+      color: "blackwhite",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066710/Images%20for%20project%203/bw8_w0ydi8.jpg"
+    },
+    {
+      name: "Black and White 9",
+      color: "blackwhite",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066711/Images%20for%20project%203/bw9_tkhhd0.jpg"
+    },
+    {
+      name: "Black and White 10",
+      color: "blackwhite",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066709/Images%20for%20project%203/bw10_cjdmyk.jpg"
+    },
+    {
+      name: "Black and White 11",
+      color: "blackwhite",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066709/Images%20for%20project%203/bw11_yriyo1.jpg"
+    },
+    {
+      name: "Black and White 12",
+      color: "blackwhite",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066712/Images%20for%20project%203/bw12_nvwtyw.jpg"
+    },
+    {
+      name: "Black and White 13",
+      color: "blackwhite",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066711/Images%20for%20project%203/bw13_pn3l2w.jpg"
+    },
+    {
+      name: "Black and White 14",
+      color: "blackwhite",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066710/Images%20for%20project%203/bw14_ljynlp.jpg"
+    },
+    {
+      name: "Black and White 15",
+      color: "blackwhite",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066710/Images%20for%20project%203/bw15_dsf4bu.jpg"
+    },
+    {
+      name: "Black and White 16",
+      color: "blackwhite",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066710/Images%20for%20project%203/bw16_yay43u.jpg"
+    },
+    {
+      name: "Black and White 17",
+      color: "blackwhite",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066710/Images%20for%20project%203/bw17_qhsewl.jpg"
+    },
+    {
+      name: "Black and White 18",
+      color: "blackwhite",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066710/Images%20for%20project%203/bw18_i8xsd4.jpg"
+    },
+    {
+      name: "Blue 1",
       color: "blue",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/a/a8/Hyacinthoides_non-scripta_%28Common_Bluebell%29.jpg"
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066719/Images%20for%20project%203/blue1_rp3a2j.jpg"
     },
     {
-      name: "Rose",
-      color: "red",
-      image: "https://www.jacksonandperkins.com/images/xxl/v1780.jpg"
-    }
+      name: "Blue 2",
+      color: "blue",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066718/Images%20for%20project%203/blue2_e3u4cv.jpg"
+    },
+    {
+      name: "Blue 3",
+      color: "blue",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066719/Images%20for%20project%203/blue3_wbk3ps.jpg"
+    },
+    {
+      name: "Blue 4",
+      color: "blue",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066718/Images%20for%20project%203/blue4_zhqrgu.jpg"
+    },
+    {
+      name: "Blue 5",
+      color: "blue",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066719/Images%20for%20project%203/blue5_ldjdrv.jpg"
+    },
+    {
+      name: "Blue 6",
+      color: "blue",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066719/Images%20for%20project%203/blue6_m45soo.jpg"
+    },
+    {
+      name: "Blue 7",
+      color: "blue",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066719/Images%20for%20project%203/blue7_e93z43.jpg"
+    },
+    {
+      name: "Blue 8",
+      color: "blue",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066719/Images%20for%20project%203/blue8_ahpyet.jpg"
+    },
+    {
+      name: "Blue 9",
+      color: "blue",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066719/Images%20for%20project%203/blue9_w9kyuy.jpg"
+    },
+    {
+      name: "Blue 10",
+      color: "blue",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066719/Images%20for%20project%203/blue10_zu4ba8.jpg"
+    },
+    {
+      name: "Blue 11",
+      color: "blue",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066719/Images%20for%20project%203/blue11_fleks3.jpg"
+    },
+    {
+      name: "Blue 12",
+      color: "blue",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066719/Images%20for%20project%203/blue12_mlnat6.jpg"
+    },
+    {
+      name: "Brown 1",
+      color: "brown",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066731/Images%20for%20project%203/br1_hymcpo.jpg"
+    },
+    {
+      name: "Brown 2",
+      color: "brown",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066731/Images%20for%20project%203/br2_mercnn.jpg"
+    },
+    {
+      name: "Brown 3",
+      color: "brown",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066731/Images%20for%20project%203/br3_pejus9.jpg"
+    },
+    {
+      name: "Brown 4",
+      color: "brown",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066732/Images%20for%20project%203/br4_uolpyw.jpg"
+    },
+    {
+      name: "Brown 5",
+      color: "brown",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066732/Images%20for%20project%203/br5_f8qglm.jpg"
+    },
+    {
+      name: "Brown 6",
+      color: "brown",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066732/Images%20for%20project%203/br6_gitsko.jpg"
+    },
+    {
+      name: "Brown 7",
+      color: "brown",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066732/Images%20for%20project%203/br7_csuyca.jpg"
+    },
+    {
+      name: "Brown 8",
+      color: "brown",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066732/Images%20for%20project%203/br8_xfbygn.jpg"
+    },
+    {
+      name: "Brown 9",
+      color: "brown",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066733/Images%20for%20project%203/br9_b0fcni.jpg"
+    },
+    {
+      name: "Brown 10",
+      color: "brown",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066733/Images%20for%20project%203/br10_zlyprk.jpg"
+    },
+    {
+      name: "Brown 11",
+      color: "brown",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066732/Images%20for%20project%203/br11_kvchsj.jpg"
+    },
+    {
+      name: "Brown 12",
+      color: "brown",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066730/Images%20for%20project%203/br12_ura8br.jpg"
+    },
+    {
+      name: "Brown 13",
+      color: "brown",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066730/Images%20for%20project%203/br13_szpcnq.jpg"
+    },
+    {
+      name: "Brown 14",
+      color: "brown",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066730/Images%20for%20project%203/br14_tfkavd.jpg"
+    },
+    {
+      name: "Brown 15",
+      color: "brown",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066730/Images%20for%20project%203/br15_dxepik.jpg" 
+    },
+    {
+      name: "Brown 16",
+      color: "brown",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066730/Images%20for%20project%203/br16_cr1vik.jpg"
+    },
+    {
+      name: "Brown 17",
+      color: "brown",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066730/Images%20for%20project%203/br17_dnfecv.jpg"
+    },
+    {
+      name: "Brown 18",
+      color: "brown",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066731/Images%20for%20project%203/br18_tvrk3a.jpg"
+    },
+    {
+      name: "Brown 19",
+      color: "brown",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066731/Images%20for%20project%203/br19_wcenvy.jpg"
+    },
+    {
+      name: "Brown 20",
+      color: "brown",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066731/Images%20for%20project%203/br20_hgd2kg.jpg"
+    },
+    {
+      name: "Brown 21",
+      color: "brown",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066731/Images%20for%20project%203/br21_chpdfj.jpg"
+    },
+    {
+      name: "Brown 22",
+      color: "brown",
+      image: "https://res.cloudinary.com/duqpl2rsg/image/upload/v1652066731/Images%20for%20project%203/br22_von9jv.jpg"
+    },
   ];
+
+  const ul = document.querySelector('ul')
+
+   // RENDER TEXTURES TO WEBPAGE
+
+  function renderTexturesToPage(textures){
+    for(i=0; 1 < textures.length; i++){
+        let list_item = document.createElement("li");
+
+        // texture name
+        let title = document.createElement("h4");
+        title.textContent = textures[i].name;
+        // texture color
+        let color = document.createElement("p");
+        color.textContent = textures[i].color;
+        // textualigre image
+        let image = document.createElement("img");
+        image.setAttribute("src", textures[i].image)
+
+        ul.appendChild(list_item)
+        list_item.appendChild(title)
+        list_item.appendChild(color)
+        list_item.appendChild(image)
+    }
+  }
+
+  renderTexturesToPage(textures)
+
+  // FILTER TEXTURES BY COLOR
+
+  const filterBtns = document.querySelector('.filters')
